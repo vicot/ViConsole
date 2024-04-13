ebe4494ec808030e8c39208e4fc8696f00f66a5c
Vicot <tarkamo@gmail.com>
2024-04-13 03:26:19 +0200

    .

--- Assets/ViConsole/Resources/ViConsole/UIToolkit/ConsoleInputControl.cs
+++ Assets/ViConsole/Resources/ViConsole/UIToolkit/ConsoleInputControl.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
@@ -13,6 +14,7 @@ namespace ViConsole.UIToolkit
 
         int _lastSelectIndex = 0;
         int _lastCursorIndex = 0;
+        int _lastTaggedLength = 0;
         string _command = "";
         Dictionary<int, int> _indexReMap = new();
 
@@ -27,98 +29,102 @@ namespace ViConsole.UIToolkit
 
         void OnGenerateVisualContent(MeshGenerationContext obj)
         {
-            _lastCursorIndex = _textElement.selection.cursorIndex;
-            _lastSelectIndex = _textElement.selection.selectIndex;
+            _lastCursorIndex = cursorIndex;
+            _lastSelectIndex = selectIndex;
 
             Debug.Log(_textElement.selection.cursorIndex);
             Debug.Log(_textElement.selection.selectIndex);
-        }
-
-        private string FormatText(string txt)
-        {
-            var sb = new StringBuilder();
-            _indexReMap.Clear();
-
-            var parts = txt.Split(" ", 3);
-            if (parts.Length >= 1)
-            {
-                sb.AppendFormat("<color=red><noparse>{0}</noparse></color>", parts[0]);
-                _indexReMap[0] = "<color=red><noparse>".Length;
-                var idx = parts[0].Length;
-                _indexReMap[idx] = "</noparse></color>".Length;
-            }
-
-            if (parts.Length >= 2)
-            {
-                var idx = parts[0].Length + 1;
-                sb.AppendFormat(" <b><noparse>{0}</noparse></b>", parts[1]);
-                _indexReMap[idx] = "<b><noparse>".Length;
-                idx += parts[1].Length;
-                _indexReMap[idx] = "</noparse></b>".Length;
-            }
-
-            if (parts.Length >= 3)
-            {
-                var idx = parts[0].Length + 1 + parts[1].Length + 1;
-                sb.AppendFormat(" <i><noparse>{0}</noparse></i>", parts[2]);
-                _indexReMap[idx] = "<i><noparse>".Length;
-                idx += parts[2].Length;
-                _indexReMap[idx] = "</noparse></i>".Length;
-            }
-
-            return sb.ToString();
         }
 
         void OnValueChanged(ChangeEvent<string> args)
         {
-            Debug.Log(_textElement.selection.cursorIndex);
-            Debug.Log(_textElement.selection.selectIndex);
+            // Debug.Log(_textElement.selection.cursorIndex);
+            // Debug.Log(_textElement.selection.selectIndex);
+            var lastStart = _lastSelectIndex;
+            var lastEnd = _lastCursorIndex;
+            if (lastStart > lastEnd) (lastStart, lastEnd) = (lastEnd, lastStart);
 
-            // var cursorIndex = _textElement.selection.cursorIndex;
-            // var selectIndex = _textElement.selection.selectIndex;
-            if (_lastCursorIndex == _lastSelectIndex)
+            var newIndex = cursorIndex;
+            var newValue = args.newValue;
+
+            // special cases of </noparse> input
+            if (lastStart == lastEnd && newIndex < _lastCursorIndex && _lastCursorIndex - newIndex > 1)
             {
-                if (cursorIndex < _lastCursorIndex)
+                HandleInsert(ref _command, newValue, _lastCursorIndex);
+            }
+            else
+            {
+                // If anything is selected, it will be removed and replaced 
+                if (lastStart != lastEnd)
                 {
-                    HandleBackDelete(ref _command, cursorIndex);
+                    Debug.Log($"Removing start: {lastStart} - {lastEnd} ({lastEnd - lastStart}");
+                    _command = _command.Remove(lastStart, lastEnd - lastStart);
                 }
-                else if (cursorIndex > _lastCursorIndex)
+
+                if (newIndex < lastStart)
                 {
-                    if (cursorIndex - _lastCursorIndex > 1)
-                        HandlePaste(ref _command, args.newValue, cursorIndex);
+                    HandleBackDelete(ref _command, newIndex);
+                }
+                else if (newIndex > lastStart)
+                {
+                    if (newIndex - lastStart > 1)
+                        HandlePaste(ref _command, newValue, lastStart, newIndex - lastStart);
                     else
-                        HandleInsert(ref _command, args.newValue);
+                        HandleInsert(ref _command, newValue, lastStart);
                 }
-                else if (cursorIndex == _lastCursorIndex)
+                else if (newIndex == lastStart)
                 {
-                    HandleFrontDelete();
+                    HandleFrontDelete(ref _command, newIndex);
                 }
             }
 
-            SetValueWithoutNotify(FormatText(_command));
+            int noparse = _command.IndexOf("</noparse>", StringComparison.OrdinalIgnoreCase);
+            while (noparse >= 0)
+            {
+                _command = _command.Remove(noparse, "</noparse>".Length);
+                cursorIndex = noparse;
+                selectIndex = cursorIndex;
+                noparse = _command.IndexOf("</noparse>", StringComparison.OrdinalIgnoreCase);
+            }
+
+            var taggedCommand = FormatText(_command);
+            _lastTaggedLength = taggedCommand.Length;
+            SetValueWithoutNotify(taggedCommand);
         }
 
-        void HandlePaste(ref string command, string newValue, int i)
+        void HandlePaste(ref string command, string newValue, int index, int length)
         {
-        }
+            var remappedIndex = RemapIndex(index);
 
-        void HandleFrontDelete()
-        {
-        }
-
-        void HandleInsert(ref string command, string newValue)
-        {
-            var remappedIndex = RemapIndex(_lastCursorIndex);
-
-            var inserted = newValue[remappedIndex];
+            var inserted = newValue[remappedIndex..(remappedIndex + length)];
             Debug.Log(inserted);
+            command = command.Insert(index, inserted);
+        }
 
-            command = command.Insert(_lastCursorIndex, inserted.ToString());
+        void HandleFrontDelete(ref string command, int cursorIndex)
+        {
+            if (cursorIndex >= command.Length)
+                return;
+
+            command = command.Remove(cursorIndex, 1);
         }
 
         void HandleBackDelete(ref string command, int cursorIndex)
         {
+            if (cursorIndex >= command.Length)
+                return;
+
             command = command.Remove(cursorIndex, 1);
+        }
+
+        void HandleInsert(ref string command, string newValue, int index)
+        {
+            var remappedIndex = RemapIndex(index);
+
+            var inserted = newValue[remappedIndex];
+            Debug.Log(inserted);
+
+            command = command.Insert(index, inserted.ToString());
         }
 
         int RemapIndex(int index)
@@ -137,6 +143,50 @@ namespace ViConsole.UIToolkit
         private string ReplaceAt(string str, int position, string c)
         {
             return str[..position] + c + str[(position + 1)..];
+        }
+
+        private string FormatText(string txt)
+        {
+            var sb = new StringBuilder();
+            _indexReMap.Clear();
+
+            var parts = txt.Split(" ", 3);
+            if (parts.Length >= 1)
+            {
+                sb.AppendFormat("<color=red><noparse>{0}</noparse></color>", parts[0]);
+                if (parts[0].Length > 0)
+                {
+                    _indexReMap[0] = "<color=red><noparse>".Length;
+                    var idx = parts[0].Length;
+                    _indexReMap[idx] = "</noparse></color>".Length;
+                }
+            }
+
+            if (parts.Length >= 2)
+            {
+                var idx = parts[0].Length + 1;
+                sb.AppendFormat(" <b><noparse>{0}</noparse></b>", parts[1]);
+                if (parts[1].Length > 0)
+                {
+                    _indexReMap[idx] = "<b><noparse>".Length;
+                    idx += parts[1].Length;
+                    _indexReMap[idx] = "</noparse></b>".Length;
+                }
+            }
+
+            if (parts.Length >= 3)
+            {
+                var idx = parts[0].Length + 1 + parts[1].Length + 1;
+                sb.AppendFormat(" <i><noparse>{0}</noparse></i>", parts[2]);
+                if (parts[2].Length > 0)
+                {
+                    _indexReMap[idx] = "<i><noparse>".Length;
+                    idx += parts[2].Length;
+                    _indexReMap[idx] = "</noparse></i>".Length;
+                }
+            }
+
+            return sb.ToString();
         }
     }
 }
